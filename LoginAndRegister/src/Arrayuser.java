6ece92d567339dd86ccd45e59e07142629875562
Kenzbvuma <104380963+Kenzbvuma@users.noreply.github.com>
2022-07-03 11:21:41 +0200

    Changed if to switch case
    PROGPOE

--- LoginAndRegister/src/Arrayuser.java
+++ LoginAndRegister/src/Arrayuser.java
@@ -46,101 +46,101 @@ public class Arrayuser {
         };
         Taskuser taskobj = new Taskuser();
         //populate array
-        if (choice == 1) {
-
-            for (int i = 0; i < name.length; i++) {
-                System.out.println("Enter a task name:");
-
-                name[i] = kb.nextLine();
-
-                taskNr[i] = i;
-                //Checking task description
-                System.out.println("Enter task description:");//prompts user for task description
-                desc[i] = kb.nextLine();
-
-                System.out.println("Enter developer details:");// prompts user for Developer details
-                devDetes[i] = kb.nextLine();
-
-                System.out.println("Enter task duration");// prompts user to display duration the task runs
-                duration[i] = kb.nextDouble();
-                kb.nextLine();//consumes enter key
-
-                System.out.println("Enter task status:"
-                        + "\n 1. Done"
-                        + "\n 2. Doing"
-                        + "\n 3.To Do ");// Prompts user to enter task status
-                int statuschoice = kb.nextInt();
-                if (statuschoice == 1) {
-                    status[i] = "done";
-                } else if (statuschoice == 2) {
-                    status[i] = "Doing";
-                } else {
-                    status[i] = " To Do";
+        switch (choice) {
+            case 1:
+                for (int i = 0; i < name.length; i++) {
+                    System.out.println("Enter a task name:");
+                    
+                    name[i] = kb.nextLine();
+                    
+                    taskNr[i] = i;
+                    //Checking task description
+                    System.out.println("Enter task description:");//prompts user for task description
+                    desc[i] = kb.nextLine();
+                    
+                    System.out.println("Enter developer details:");// prompts user for Developer details
+                    devDetes[i] = kb.nextLine();
+                    
+                    System.out.println("Enter task duration");// prompts user to display duration the task runs
+                    duration[i] = kb.nextDouble();
+                    kb.nextLine();//consumes enter key
+                    
+                    System.out.println("Enter task status:"
+                            + "\n 1. Done"
+                            + "\n 2. Doing"
+                            + "\n 3.To Do ");// Prompts user to enter task status
+                    int statuschoice = kb.nextInt();
+                    if (statuschoice == 1) {
+                        status[i] = "done";
+                    } else if (statuschoice == 2) {
+                        status[i] = "Doing";
+                    } else {
+                        status[i] = " To Do";
+                    }
+                    
+                    kb.nextLine();//consumes enter key
+                    // ( Farrell , 2019)
+                    System.out.println(
+                            name[i] + "\n"
+                                    + desc[i] + "\n"
+                                    + devDetes[i] + "\n"
+                                    + duration[i] + "\n"
+                                    + status[i]);
+                    
+                }   taskobj.checkTaskDescription(desc);
+                taskId = taskobj.creatTaskID(name, taskNr, devDetes, userInput);
+                taskobj.returnTotalHours(duration);
+                int userchoice = 0;
+                while (userchoice != 7) {
+                    
+                    System.out.println("Display"
+                            + " 1.Display the Developer, Task Names and Task Duration for all tasks with the status of done.\n"
+                            + "2.Display the Developer and Duration of the class with the longest duration.\n"
+                            + "3.Search for a task with a Task Name and display the Task Name, Developer and Task Status.\n"
+                            + "4.Search for all tasks assigned to a developer and display the Task Name and Task Status.\n"
+                            + "5.Delete a task using the Task Name.\n"
+                            + "6.Display a report that lists the full details of all captured tasks.\n"
+                            + "7.QUIT");
+                    
+                    userchoice = kb.nextInt();
+                    kb.nextLine();//consumes enter key
+                    // User choice for searching aRRAYS
+                    if (userchoice == 1) {
+                        displayStatusDone(devDetes, name, duration, status);
+                    } else if (userchoice == 2) {
+                        displaylongduration(devDetes, duration);
+                    } else if (userchoice == 3) {
+                        System.out.println("Enter name you want to search for");
+                        String tempsearchtsk = kb.next();
+                        SearchTask(name, devDetes, status, tempsearchtsk);
+                    } else if (userchoice == 4) {
+                        System.out.println("Enter Developer Name ");
+                        String searchdev = kb.next();
+                        Searchdevnametasks(devDetes, name, status, searchdev);
+                    } else if (userchoice == 5) {
+                        System.out.println("Enter task name you want to delete");
+                        String searchdelete = kb.next();
+                        devDetes = SearchDelete(devDetes, name, searchdelete);
+                        status = SearchDelete(status, name, searchdelete);
+                        taskId = SearchDelete(taskId, name, searchdelete);
+                        duration = deleteduration(duration, name, searchdelete);
+                        name = SearchDelete(name, name, searchdelete);
+                    } else if (userchoice == 6) {
+                        displayAllArrays(devDetes,name,taskId,duration,status,taskNr);
+                        
+                    } else if (userchoice == 7) {
+                        System.exit(0);
+                    }
+                    
                 }
-
-                kb.nextLine();//consumes enter key
-                // ( Farrell , 2019)
-                System.out.println(
-                        name[i] + "\n"
-                        + desc[i] + "\n"
-                        + devDetes[i] + "\n"
-                        + duration[i] + "\n"
-                        + status[i]);
-
-            }
-            taskobj.checkTaskDescription(desc);
-            taskId = taskobj.creatTaskID(name, taskNr, devDetes, userInput);
-            taskobj.returnTotalHours(duration);
-
-            int userchoice = 0;
-            while (userchoice != 7) {
-
-                System.out.println("Display"
-                        + " 1.Display the Developer, Task Names and Task Duration for all tasks with the status of done.\n"
-                        + "2.Display the Developer and Duration of the class with the longest duration.\n"
-                        + "3.Search for a task with a Task Name and display the Task Name, Developer and Task Status.\n"
-                        + "4.Search for all tasks assigned to a developer and display the Task Name and Task Status.\n"
-                        + "5.Delete a task using the Task Name.\n"
-                        + "6.Display a report that lists the full details of all captured tasks.\n"
-                        + "7.QUIT");
-
-                userchoice = kb.nextInt();
-                kb.nextLine();//consumes enter key
-                // User choice for searching aRRAYS 
-                if (userchoice == 1) {
-                    displayStatusDone(devDetes, name, duration, status);
-                } else if (userchoice == 2) {
-                    displaylongduration(devDetes, duration);
-                } else if (userchoice == 3) {
-                    System.out.println("Enter name you want to search for");
-                    String tempsearchtsk = kb.next();
-                    SearchTask(name, devDetes, status, tempsearchtsk);
-                } else if (userchoice == 4) {
-                    System.out.println("Enter Developer Name ");
-                    String searchdev = kb.next();
-                    Searchdevnametasks(devDetes, name, status, searchdev);
-                } else if (userchoice == 5) {
-                    System.out.println("Enter task name you want to delete");
-                    String searchdelete = kb.next();
-                    devDetes = SearchDelete(devDetes, name, searchdelete);
-                    status = SearchDelete(status, name, searchdelete);
-                    taskId = SearchDelete(taskId, name, searchdelete);
-                    duration = deleteduration(duration, name, searchdelete);
-                    name = SearchDelete(name, name, searchdelete);
-                } else if (userchoice == 6) {
-                    displayAllArrays(devDetes,name,taskId,duration,status,taskNr);
-
-                } else if (userchoice == 7) {
-                    System.exit(0);
-                }
-
-            }
-            // array.toString();
-        } else if (choice == 2) {
-
-            JOptionPane.showMessageDialog(null, "Coming Soon");
-        } else {
-            JOptionPane.showMessageDialog(null, "Quit");
+                // array.toString();
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null, "Coming Soon");
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Quit");
+                break;
         }
 
     }
